b001b040685f939617b3c91066462ad9812a52cd
Piotr Szul <piotr.szul@csiro.au>
2024-05-31 09:26:37 +1000

    Adding script to validata a subsample of mimic-iv-fhir-demo patient data.

---
+++ scripts/validate_patients.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Validate the patient data
+ROOT_DIR="$(dirname $0)/.."
+
+spark-submit --driver-memory 4g --master local[*] \
+    $ROOT_DIR/target/spark-validator-1.0-SNAPSHOT-all.jar \
+    $ROOT_DIR/data/mimic-iv-10/MimicPatient.ndjson \
+    $ROOT_DIR/target/MimicPatient_validation.parquet
